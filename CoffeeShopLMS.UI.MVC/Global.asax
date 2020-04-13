619ae3453ff64221fea020eb6adca0b729993d2c
willis-aj <willis.aj@outlook.com>
2020-04-13 13:25:29 -0500

    Initial Commit

---
+++ CoffeeShopLMS.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoffeeShopLMS.UI.MVC.MvcApplication" Language="C#" %>
---
+++ CoffeeShopLMS.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeShopLMS.UI.MVC.Startup))]
+namespace CoffeeShopLMS.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
